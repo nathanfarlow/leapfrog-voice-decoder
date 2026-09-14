8608d900d1518a9c643489c1af669980af307997
Nathan Farlow <natethegreatest11@gmail.com>
2026-09-14 19:29:46 -0400

    Fix convert all on web

--- lib/rom.ml
+++ lib/rom.ml
@@ -2,7 +2,7 @@ open! Core
 
 (* Fuzzy find the big table of voice streams.
 
-   The streams have the structure:
+   The table has the structure:
    id, 0x0100, 0, flags, count followed by [count] voice pointers.
 
    And we also know all voice streams must start with a half word of 0, 1, or 2. *)
@@ -13,13 +13,14 @@ let stream_offsets rom =
   let u32 pos = Iobuf.Peek.uint32_le rom ~pos in
   let table_at off =
     let magic = u32 (off + 4) in
+    let zero = u32 (off + 8) in
     let count = u32 (off + 16) in
-    if magic <> 0x0100 || count >= 0x4000
+    if magic <> 0x0100 || zero <> 0 || count <= 0 || count >= 0x4000
     then None
     else (
-      let cart_base = 0x80000000 in
+      let base = u32 (off + 20) land lnot 0xFFFFFF in
       let offsets =
-        List.init count ~f:(fun i -> u32 (off + 20 + (4 * i)) - cart_base)
+        List.init count ~f:(fun i -> u32 (off + 20 + (4 * i)) - base)
         |> List.dedup_and_sort ~compare:Int.compare
       in
       let is_stream pos =
